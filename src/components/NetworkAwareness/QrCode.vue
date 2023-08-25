52eab2af8e54193c71488973ee36f51edaffcb2e
Prabhjot Singh <pjsingh1359@gmail.com>
2023-08-25 11:17:21 -0700

    display qrcode when vcf file is provided

--- src/components/NetworkAwareness/QrCode.vue
+++ src/components/NetworkAwareness/QrCode.vue
@@ -1,13 +1,56 @@
+<!--
+  Contributor: PJ Singh
+
+  Description: This component is used to generate a QR code from a vCard file (.vcf).
+ -->
 <template>
-  <div class="main">
-    <h1 class="view-name">
-      <span class="font-bold">QR Code</span>
-    </h1>
+  <div class="flex flex-col items-center">
+    <input type="file" @change="handleFileChange" />
+    <!-- generate if qrCodeDataUrl is filled -->
+    <div class="flex mg">
+      <img v-if="qrCodeDataUrl" :src="qrCodeDataUrl" alt="QR Code" />
+      <p v-else>Upload a VCard to generate a QR Code</p>
+    </div>
   </div>
 </template>
 
 <script>
+import vCardParser from 'vcard-parser'
+import QRCode from 'qrcode'
+
 export default {
-  name: 'QrCode'
+  name: 'QrCode',
+  data() {
+    return {
+      qrCodeDataUrl: ''
+    }
+  },
+  methods: {
+    async handleFileChange(event) {
+      const file = event.target.files[0]
+
+      if (file) {
+        // Check if file is a .vcf file (vCard) and alert user if it is not.
+        if (!file.name.toLowerCase().endsWith('.vcf')) {
+          alert('Please select a vCard file (.vcf)')
+          this.qrCodeDataUrl = ''
+          return
+        }
+        // Read file
+        const reader = new FileReader()
+        reader.onload = async (e) => {
+          // Parse vCard
+          const content = e.target.result
+          const parsedData = vCardParser.parse(content)
+          const vCardText = vCardParser.generate(parsedData)
+
+          // Generate QR Code
+          const qrCodeCanvas = await QRCode.toCanvas(vCardText)
+          this.qrCodeDataUrl = qrCodeCanvas.toDataURL()
+        }
+        reader.readAsText(file)
+      }
+    }
+  }
 }
 </script>
