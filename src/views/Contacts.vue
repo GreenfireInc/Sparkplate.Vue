f79ebfc8e5c42696797dae3001aee0ecde10471a
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-09-04 11:20:38 -0700

    add a table to the /views/Contacts.vue to show a list of contacts.

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="view">Contacts</div>
+  <div class="view">
+    <h1 class="view-name">Contacts</h1>
+    <div class="bg-white shadow py-5 px-4 mt-4">
+      <table class="table">
+        <thead class="container">
+          <tr class="row">
+            <th class="col">Name</th>
+          </tr>
+        </thead>
+        <tbody class="container">
+          <tr class="row">
+            <td class="col">Static Assets</td>
+          </tr>
+          <tr class="row">
+            <td class="col">Electron Background Process Entrypoint</td>
+          </tr>
+          <tr class="row">
+            <td class="col">Vue Renderer Entrypoint</td>
+          </tr>
+          <tr class="row">
+            <td class="col">Main Application Component</td>
+          </tr>
+          <tr class="row">
+            <td class="col">Side Navigation Component</td>
+          </tr>
+          <tr class="row">
+            <td class="col">Test Page Component</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
