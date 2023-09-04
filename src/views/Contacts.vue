6744df4a8eb2759efffd69cfe3d97e2eac14d627
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-09-04 13:06:18 -0700

    experimenting with adding a modal to the /view/Contact.vue.

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -4,28 +4,28 @@
     <div class="bg-white shadow py-5 px-4 mt-4">
       <table class="table">
         <thead class="container">
-          <tr class="row">
+          <tr class="clickable-row">
             <th class="col">Name</th>
           </tr>
         </thead>
         <tbody class="container">
-          <tr class="row">
-            <td class="col">Static Assets</td>
+          <tr class="clickable-row">
+            <td class="col">John Doe</td>
           </tr>
-          <tr class="row">
-            <td class="col">Electron Background Process Entrypoint</td>
+          <tr class="clickable-row">
+            <td class="col">Jack Ryan</td>
           </tr>
-          <tr class="row">
-            <td class="col">Vue Renderer Entrypoint</td>
+          <tr class="clickable-row">
+            <td class="col">Lara Croft</td>
           </tr>
-          <tr class="row">
-            <td class="col">Main Application Component</td>
+          <tr class="clickable-row">
+            <td class="col">Soap McTavish</td>
           </tr>
-          <tr class="row">
-            <td class="col">Side Navigation Component</td>
+          <tr class="clickable-row">
+            <td class="col">John Price</td>
           </tr>
-          <tr class="row">
-            <td class="col">Test Page Component</td>
+          <tr class="clickable-row">
+            <td class="col">Handler Walter</td>
           </tr>
         </tbody>
       </table>
