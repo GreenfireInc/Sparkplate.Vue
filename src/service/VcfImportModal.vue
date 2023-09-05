f5dca0d0774bc914e1030ac07fef4b1f64e4aad4
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-09-04 18:20:33 -0700

    add code to handle a fileUpload for the vcfImportModal.

--- src/service/VcfImportModal.vue
+++ src/service/VcfImportModal.vue
@@ -1,14 +1,25 @@
-<!-- <template>
-
-</template> -->
+<template>
+  <div class="vcf-import-modal">
+    <div class="modal-content">
+      <h2>Import VCF File</h2>
+      <input type="file" @change="handleFileUpload" accept=".vcf" />
+      <div v-if="vcfData">
+        <h3>Contact Information</h3>
+        <pre>{{ vcfData }}</pre>
+        <qrcode-vue :value="vcfData"></qrcode-vue>
+      </div>
+      <button @click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
 
 <script>
-import { QrCodeVue } from 'vue-qrcode'
+import { QrcodeVue } from 'vue-qrcode'
 
 export default {
   name: 'VcfImportModal',
   components: {
-    QrCodeVue
+    QrcodeVue
   },
   data() {
     return {
@@ -16,7 +27,7 @@ export default {
     }
   },
   methods: {
-    handleFileUploadEvent(event) {
+    handleFileUpload(event) {
       const file = event.target.files[0]
       if (file) {
         const reader = new FileReader()
@@ -27,7 +38,36 @@ export default {
 
         reader.readAsText(file)
       }
+    },
+    closeModal() {
+      this.$emit('close')
     }
   }
 }
 </script>
+
+<!-- <style scoped>
+.vcf-import-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+button {
+  margin-top: 10px;
+}
+</style> -->
