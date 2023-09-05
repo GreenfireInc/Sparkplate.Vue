1d7d3d9b254a4cf27b69f0f25246c993f517e700
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-09-04 17:53:29 -0700

    add a modal service to handle importing vcf files to convey them as QR codes.

---
+++ src/service/VcfImportModal.vue
@@ -0,0 +1,33 @@
+<!-- <template>
+
+</template> -->
+
+<script>
+import { QrCodeVue } from 'vue-qrcode'
+
+export default {
+  name: 'VcfImportModal',
+  components: {
+    QrCodeVue
+  },
+  data() {
+    return {
+      vcfData: null
+    }
+  },
+  methods: {
+    handleFileUploadEvent(event) {
+      const file = event.target.files[0]
+      if (file) {
+        const reader = new FileReader()
+
+        reader.onload = (e) => {
+          this.vcfData = e.target.result
+        }
+
+        reader.readAsText(file)
+      }
+    }
+  }
+}
+</script>
